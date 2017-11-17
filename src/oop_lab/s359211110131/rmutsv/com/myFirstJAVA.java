c8d774c4f2334050d3c53951e6f5c41494ba3623
wiparat seesatja <tuktikt37@gmail.com>
2017-11-17 10:32:31 +0700

    Initial upload to GitHub

---
+++ src/oop_lab/s359211110131/rmutsv/com/myFirstJAVA.java
@@ -0,0 +1,8 @@
+package oop_lab.s359211110131.rmutsv.com;
+
+public class myFirstJAVA {
+    public static void main(String[] args) {
+        System.out.println("Hello Word, I'm Wiparat");
+    }
+
+}
