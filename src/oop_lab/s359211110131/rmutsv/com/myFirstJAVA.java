f145f27792dad53d4b719378b9218fa7c8eee255
wiparat seesatja <tuktikt37@gmail.com>
2017-11-17 11:14:13 +0700

    Successfully information data

--- src/oop_lab/s359211110131/rmutsv/com/myFirstJAVA.java
+++ src/oop_lab/s359211110131/rmutsv/com/myFirstJAVA.java
@@ -3,6 +3,9 @@ package oop_lab.s359211110131.rmutsv.com;
 public class myFirstJAVA {
     public static void main(String[] args) {
         System.out.println("Hello Word, I'm Wiparat");
+        System.out.println("Information System");
+        System.out.println("Faculty of Management Technolgy");
+        System.out.println("RMUTSV");
     }
 
 }
