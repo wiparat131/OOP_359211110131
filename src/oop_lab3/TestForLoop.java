e4c4a090b4fb4e165d8f720a41baaf2dfe2507d9
LAB_COM-MT_1 <tuktikt37@gmail.com>
2017-12-11 15:17:54 +0700

    Test ForLoop

---
+++ src/oop_lab3/TestForLoop.java
@@ -0,0 +1,18 @@
+package oop_lab3;
+
+public class TestForLoop {
+    public static void main(String[] args) {
+//        for loop
+//        for (int i=1;i<=10;i++){
+//        System.out.println("Hello");
+//        System.out.println("IS223");
+//    }
+
+          for (int i=1;i<=100;i++) {
+              if (i%3==0 && i%5==0)
+              System.out.print(i+" ");
+          }
+    } // main
+
+} // class
+
