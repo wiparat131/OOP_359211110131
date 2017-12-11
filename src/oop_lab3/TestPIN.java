71d369a7b10f8a29646cad401d6bcb39931260e5
LAB_COM-MT_1 <tuktikt37@gmail.com>
2017-12-11 15:52:14 +0700

    Test WhileLoop

---
+++ src/oop_lab3/TestPIN.java
@@ -0,0 +1,19 @@
+package oop_lab3;
+
+import java.util.Scanner;
+
+public class TestPIN {
+    public static void main(String[] args) {
+//        Scanner
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input your PIN : ");
+
+        int PIN=3;
+        do {
+            if (PIN)
+                System.out.println("Welcome");
+        } while
+            ("Locked");
+
+    }
+}
