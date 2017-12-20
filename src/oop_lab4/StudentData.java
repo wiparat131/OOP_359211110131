23f9367afeb5cd75cbb5d5c0b956faa111950996
Acer <tuktikt37@gmail.com>
2017-12-20 23:37:41 +0700

    Student Data

--- src/oop_lab4/StudentData.java
+++ src/oop_lab4/StudentData.java
@@ -1,30 +1,57 @@
 package oop_lab4;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 //1.ชื่อ-สกุล
 //2.รหัสนักศึกษา
 //3.สาขาที่เรียน และคณะ
 //4.ที่อยู่
 //5.email
 public abstract class StudentData {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        static BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+        public static void main(String[] args) throws IOException {
+            String stdname = getStdName();
+            String stdid = getStdID();
+            String stdfac = getStdFac();
+            String stdaddress = getStdAdd();
+            String stdemail = getStdEmail();
+            System.out.println("Student name:"+stdname+
+                    "ID: "+stdid+
+                    "Major and Faculty: "+stdfac+
+                    "Address:"+stdaddress+
+                    "Email:"+stdemail);
+        }//main
 
-    public static void main(String[] args) {
-        getName();
-    }//main
+        private static String getStdEmail() throws IOException {
+            System.out.print("Enter your id: ");
+            String email = reader.readLine();
+            return email;
+        }
 
-    public static void getName() {
-        System.out.println("Wiparat Seesatja ");
-        IDName();
-    }
+        private static String getStdAdd() throws IOException {
+            System.out.print("Enter your address: ");
+            String add = reader.readLine();
+            return add;
+        }
 
-    private static void IDName() {
-        System.out.println("359211110131");
-    }
-}
+        private static String getStdFac() throws IOException {
+            System.out.print("Enter your Major and Faculty: ");
+            String fac = reader.readLine();
+            return fac;
+        }
 
+        private static String getStdID() throws IOException {
+            System.out.print("Enter your id: ");
+            String id = reader.readLine();
+            return id;
+        }
+
+        private static String getStdName() throws IOException {
+            System.out.print("Enter your name: ");
+            //String s = reader.readLine();
+            //return s;
+            return reader.readLine();
+        }
+    }//class
 
