16ed60eec4227e596d9a8965cf0a38c404382d66
LAB_COM-MT_1 <tuktikt37@gmail.com>
2017-12-29 10:10:37 +0700

    myStrToken

---
+++ src/oop_lab6/myStrToken.java
@@ -0,0 +1,20 @@
+package oop_lab6;
+
+import java.util.StringTokenizer;
+
+public class myStrToken {
+    public static void main(String[] args) {
+//        StringTokenizer
+        String msg = "Hello Welcome MT RMUTSV 2017";
+        StringTokenizer myToken = new StringTokenizer(msg);
+//        countToken
+        System.out.println(myToken.countTokens());
+        while (myToken.hasMoreTokens()){
+            StringBuffer buffer = new StringBuffer(myToken.nextToken());
+            System.out.print(buffer.reverse()+" ");
+        }//while
+
+
+
+    }//main
+}//class
