ce0baaed2b84fca89dc6c4e074219e52b52b8077
wiparat131 <Tuktikt37@gmail.com>
2018-01-26 09:05:13 +0700

    Student

--- src/oop_lab/s359211110131/rmutsv/com/myFirstJAVA.java
+++
@@ -1,9 +0,0 @@
-package oop_lab.s359211110131.rmutsv.com;
-
-public class myFirstJAVA {
-    public static void main(String[] args) {
-        System.out.println("Hello Word, I'm Wiparat");
-        System.out.println("Information System");
-    }
-
-}
