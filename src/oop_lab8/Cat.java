40c3438f6b5023bc8388b99fa3a2debc2cdfddc8
wiparat131 <Tuktikt37@gmail.com>
2018-02-02 10:38:35 +0700

    Pet

---
+++ src/oop_lab8/Cat.java
@@ -0,0 +1,13 @@
+package oop_lab8;
+
+public class Cat extends Pet {
+
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Mueaw Mueaw !!!");
+    }
+}//class
---
+++ src/oop_lab8/Dog.java
@@ -0,0 +1,16 @@
+package oop_lab8;
+
+//Dog is a cat
+//Dog is sub-class
+//Pet is super-class
+public class Dog extends Pet {
+
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Box Box !!!");
+    }
+}//class
