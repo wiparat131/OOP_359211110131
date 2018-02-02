e95ea20725ed7af8582e1823a5d37d27ebccc764
wiparat131 <Tuktikt37@gmail.com>
2018-02-02 11:01:44 +0700

    Circle

--- src/oop_lab8/Circle.java
+++ src/oop_lab8/Circle.java
@@ -1,4 +1,22 @@
 package oop_lab8;
 
-public class Circle {
-}
+public class Circle implements Shape {
+    private double r;
+
+    public Circle(double r) {
+        this.r = r;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    @Override
+    public double findArea() {
+        return 3.14*(r*r);
+    }
+}//class
--- src/oop_lab8/ShapeApp.java
+++ src/oop_lab8/ShapeApp.java
@@ -5,6 +5,9 @@ public class ShapeApp {
         Rectangle rectangle = new Rectangle(20, 30);
 
         System.out.println(rectangle.findArea());
-    }
+
+        Circle circle = new Circle(20);
+        System.out.println(circle.findArea());
+    }//main
 
 }//class
