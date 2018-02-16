638ae76730d9940a9fb7a087b5b88fa288623dc1
wiparat131 <Tuktikt37@gmail.com>
2018-02-16 09:55:28 +0700

    myCar

---
+++ src/oop_lab9/myCar.java
@@ -0,0 +1,12 @@
+package oop_lab9;
+
+public class myCar {
+    public static void main(String[] args) {
+        Engine engine = new Engine("1500 cc","V-TEC V4");
+
+        Car car = new Car("Honda","Red",engine);
+        System.out.println(car.toString());
+
+
+    }//main
+}//class
